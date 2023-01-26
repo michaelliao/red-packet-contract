ae70bf237094aaef497fae5b97a4ecfe3e583b1b
Michael Liao <askxuefeng@gmail.com>
2023-01-26 15:32:14 +0800

    refactor icons

--- gen_icons.py
+++ gen_icons.py
@@ -1,39 +1,93 @@
 #!/usr/bin/env python3
+#
+# Requirement: PyYAML
+#
+# pip3 install pyyaml
+#
 
-import os, re
-import sys
+import os, re, yaml
 import json 
 
 
 def main():
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    icon_dir = os.path.join(base_dir, 'icons')
+    icon_dir = os.path.join(base_dir, 'static', 'icons')
+
+    icon_files = list_icons(icon_dir)
+    # remove default:
+    del icon_files['default']
+    for k, v in icon_files.items():
+        print(f'found token: {k} => {v}')
+
+    with open(os.path.join(base_dir, 'tokens.yml'), 'r') as fp:
+        chains = yaml.load(fp, Loader=yaml.BaseLoader)
+
     icons = {}
-    for chain in os.listdir(icon_dir):
-        chainId = getChainId(chain)
-        if chainId > 0:
-            addrs = {}
-            icons[chain] = addrs
-            for token in os.listdir(os.path.join(icon_dir, chain)):
-                m = re.match(r'^(0x[0-9a-f]{40})\.svg$', token)
-                if m:
-                    addr = m.group(1)
-                    addrs[addr] = f'icons/{chainId}/{token}'
+    for chainName, chainInfo in chains.items():
+        chainId = chainInfo['chain']
+        addrs = {}
+        icons[chainId] = addrs
+        print(f'found chain: {chainName} => {chainId}')
+        tokens = merge_tokens(chainInfo['tokens'])
+        for token, address in tokens.items():
+            if re.match(r'^0x[0-9a-fA-F]{40}$', address):
+                if token in icon_files:
+                    addrs[address.lower()] = icon_files[token]
                 else:
-                    print(f'invalid file: {icon_dir}/{chain}/{token}')
-        else:
-            print(f'invalid chain: {chain}')
-    js = 'window.ICONS = '+ json.dumps(icons, sort_keys=True, indent=2)
+                    print(f'WARN: token {token} has no icon.');
+            else:
+                print(f'ERROR: invalid address {address} for token {token}')
+
+    js = '// auto-generated\nwindow.ICONS = '+ json.dumps(icons, indent=2)
     print(js)
-    with open('icons.js', 'w') as f:
+
+    with open(os.path.join(base_dir, 'static', 'js', 'icons.js'), 'w') as f:
         f.write(js)
 
 
-def getChainId(s):
-    try:
-        return int(s)
-    except ValueError as e:
-        return 0
+def merge_tokens(ts):
+    d = {}
+    for t in ts:
+        for k, v in t.items():
+            d[k] = v
+    return d
+
+
+def list_icons(d):
+    fs = os.listdir(d)
+    icons = {}
+    for f in fs:
+        m = re.match(r'^(\w+)\.svg$', f)
+        if m:
+            icons[m.group(1)] = f'/static/icons/{f}'
+    return icons
+
+#     icons = {}
+#     for chain in os.listdir(icon_dir):
+#         chainId = getChainId(chain)
+#         if chainId > 0:
+#             addrs = {}
+#             icons[str(chainId)] = addrs
+#             chain_dir = os.path.join(icon_dir, chain)
+#             for token in os.listdir(chain_dir):
+#                 m = re.match(r'^(0x[0-9a-fA-F]{40})\.svg$', token)
+#                 if m:
+#                     addr = m.group(1)
+#                     if token.lower() != token:
+#                         print(f'NOTE: rename {token} to {token.lower()}...')
+#                         os.rename(os.path.join(chain_dir, token), os.path.join(chain_dir, token.lower()))
+#                     addrs[addr.lower()] = f'icons/{chain}/{token.lower()}'
+#                 else:
+#                     print(f'WARN: invalid file: {icon_dir}/{chain}/{token}')
+#         else:
+#             print(f'WARN: invalid chain: {chain}')
+
+
+# def getChainId(s):
+#     m = re.match(r'^([0-9]+).*$', s)
+#     if m:
+#         return int(m.group(1))
+#     return 0
 
 
 if __name__ == '__main__':
---
+++ static/js/icons.js
@@ -0,0 +1,128 @@
+// auto-generated
+window.ICONS = {
+  "1": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/eth.svg",
+    "0xdac17f958d2ee523a2206206994597c13d831ec7": "/static/icons/usdt.svg",
+    "0xb8c77482e45f1f44de1745f52c74426c631bdd52": "/static/icons/bnb.svg",
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48": "/static/icons/usdc.svg",
+    "0x4fabb145d64652a948d72533023f6e7a623c7c53": "/static/icons/busd.svg",
+    "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0": "/static/icons/matic.svg",
+    "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce": "/static/icons/shib.svg",
+    "0x0abcfbfa8e3fda8b7fba18721caf7d5cf55cf5f5": "/static/icons/ltc.svg",
+    "0x6b175474e89094c44da98b954eedeac495271d0f": "/static/icons/dai.svg",
+    "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984": "/static/icons/uni.svg",
+    "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599": "/static/icons/wbtc.svg",
+    "0x514910771af9ca656af840dff83e8264ecf986ca": "/static/icons/link.svg",
+    "0xba3d9687cf50fe253cd2e1cfeede1d6787344ed5": "/static/icons/aave.svg",
+    "0x4e15361fd6b4bb609fa63c81a2be19d873717870": "/static/icons/ftm.svg",
+    "0x0000000000085d4780b73119b644ae5ecd22b376": "/static/icons/tusd.svg",
+    "0x8e870d67f660d95d5be530380d0ec0bd388289e1": "/static/icons/usdp.svg",
+    "0x6f259637dcd74c767781e37bc6133cd6a68aa161": "/static/icons/ht.svg",
+    "0xc944e90c64b2c07662a292be6244bdf05cda44a7": "/static/icons/grt.svg",
+    "0x056fd409e1d7a124bd7017459dfea2f387b6d5cd": "/static/icons/gusd.svg",
+    "0xc00e94cb662c3520282e6f5717214004a7f26888": "/static/icons/comp.svg",
+    "0xc18360217d8f7ab5e7c516566761ea12ce7f9d72": "/static/icons/ens.svg",
+    "0x6810e776880c02933d47db1b9fc05908e5386b96": "/static/icons/gno.svg",
+    "0xba100000625a3754423978a60c9317c58a424e3d": "/static/icons/bal.svg",
+    "0xaaaebe6fe48e54f431b0c390cfaf0b017d09d42d": "/static/icons/cel.svg",
+    "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2": "/static/icons/sushi.svg",
+    "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e": "/static/icons/yfi.svg",
+    "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2": "/static/icons/weth.svg"
+  },
+  "56": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/bnb.svg",
+    "0x2170ed0880ac9a755fd29b2688956bd959f933f8": "/static/icons/eth.svg",
+    "0x55d398326f99059ff775485246999027b3197955": "/static/icons/usdt.svg",
+    "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d": "/static/icons/usdc.svg",
+    "0xe9e7cea3dedca5984780bafc599bd69add087d56": "/static/icons/busd.svg",
+    "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3": "/static/icons/dai.svg",
+    "0x14016e85a25aeb13065688cafb43044c2ef86784": "/static/icons/tusd.svg",
+    "0xb7f8cd00c5a06c0537e2abff0b58033d02e5e094": "/static/icons/pax.svg",
+    "0xb3c11196a4f3b1da7c23d9fb0a3dde9c6340934f": "/static/icons/usdp.svg",
+    "0xf8a0bf9cf54bb92f17374d9e9a321e6a111a51bd": "/static/icons/link.svg",
+    "0x3d6545b08693dae087e957cb1180ee38b9e3c25e": "/static/icons/etc.svg",
+    "0x8ff795a6f4d97e7887c79bea79aba5cc76444adf": "/static/icons/bch.svg",
+    "0xcc42724c6683b7e57334c4e856f4c9965ed682bd": "/static/icons/matic.svg",
+    "0x4338665cbb7b2485a8855a139b75d5e34ab0db94": "/static/icons/ltc.svg",
+    "0x1ba42e5193dfa8b03d15dd1b86a3113bbbef8eeb": "/static/icons/zec.svg",
+    "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82": "/static/icons/cake.svg"
+  },
+  "128": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/ht.svg",
+    "0xa71edc38d189767582c38a3145b5873052c3e47a": "/static/icons/usdt.svg",
+    "0x64ff637fb478863b7468bc97d30a5bf3a428a1fd": "/static/icons/eth.svg",
+    "0x66a79d23e58475d2738179ca52cd0b41d73f0bea": "/static/icons/btc.svg",
+    "0xecb56cf772b5c9a6907fb7d32387da2fcbfb63b4": "/static/icons/ltc.svg",
+    "0x9362bbef4b8313a8aa9f0c9808b80577aa26b73b": "/static/icons/usdc.svg",
+    "0x3d760a45d0887dfd89a2f5385a236b29cb46ed2a": "/static/icons/dai.svg",
+    "0x5ee41ab6edd38cdfb9f6b4e6cf7f75c87e170d98": "/static/icons/tusd.svg"
+  },
+  "137": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/matic.svg",
+    "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619": "/static/icons/weth.svg",
+    "0xc2132d05d31c914a87c6611c10748aeb04b58e8f": "/static/icons/usdt.svg",
+    "0x2791bca1f2de4661ed88a30c99a7a9449aa84174": "/static/icons/usdc.svg",
+    "0x3ba4c387f786bfee076a58914f5bd38d668b42c3": "/static/icons/bnb.svg",
+    "0xdab529f40e671a1d4bf91361c21bf9f0c9712ab7": "/static/icons/busd.svg",
+    "0x2c89bbc92bd86f8075d1decc58c7f4e0107f286b": "/static/icons/avax.svg",
+    "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063": "/static/icons/dai.svg",
+    "0xb33eaad8d922b1083446dc23f610c2567fb5180f": "/static/icons/uni.svg",
+    "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6": "/static/icons/wbtc.svg",
+    "0xb0897686c545045afc77cf20ec7a532e3120e0f1": "/static/icons/link.svg",
+    "0xd6df932a45c0f255f85145f286ea0b292b21c90b": "/static/icons/aave.svg",
+    "0xc9c1c1c20b3658f8787cc2fd702267791f224ce1": "/static/icons/ftm.svg",
+    "0x2e1ad108ff1d8c782fcbbb89aad783ac49586756": "/static/icons/tusd.svg",
+    "0x6f3b3286fd86d8b47ec737ceb3d0d354cc657b3e": "/static/icons/pax.svg",
+    "0x5fe2b58c013d7601147dcdd68c143a77499f5531": "/static/icons/grt.svg",
+    "0x172370d5cd63279efa6d502dab29171933a610af": "/static/icons/crv.svg",
+    "0xc8a94a3d3d2dabc3c1caffffdca6a7543c3e3e65": "/static/icons/gusd.svg",
+    "0x8505b9d2254a7ae468c0e9dd10ccea3a837aef5c": "/static/icons/comp.svg",
+    "0x5ffd62d3c3ee2e81c00a7b9079fb248e7df024a8": "/static/icons/gno.svg",
+    "0x9a71012b13ca4d3d0cdc72a177df3ef03b0e76a3": "/static/icons/bal.svg"
+  },
+  "250": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/ftm.svg"
+  },
+  "42161": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/eth.svg"
+  },
+  "43114": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/ftm.svg"
+  },
+  "97": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/bnb.svg",
+    "0xbbb02de94e1dd1e99b43458027f31abb2d75659b": "/static/icons/wbtc.svg",
+    "0xeee0c84193e02e67164b9b4402e47ef9cffa5b09": "/static/icons/weth.svg",
+    "0xddd076e315e288a7146e8c8612a57baae4df21c7": "/static/icons/usdt.svg"
+  },
+  "5": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/eth.svg",
+    "0xbbb02de94e1dd1e99b43458027f31abb2d75659b": "/static/icons/wbtc.svg",
+    "0xeee0c84193e02e67164b9b4402e47ef9cffa5b09": "/static/icons/weth.svg",
+    "0xddd076e315e288a7146e8c8612a57baae4df21c7": "/static/icons/usdt.svg"
+  },
+  "4002": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/ftm.svg",
+    "0xbbb02de94e1dd1e99b43458027f31abb2d75659b": "/static/icons/wbtc.svg",
+    "0xeee0c84193e02e67164b9b4402e47ef9cffa5b09": "/static/icons/weth.svg",
+    "0xddd076e315e288a7146e8c8612a57baae4df21c7": "/static/icons/usdt.svg"
+  },
+  "43113": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/avax.svg",
+    "0xbbb02de94e1dd1e99b43458027f31abb2d75659b": "/static/icons/wbtc.svg",
+    "0xeee0c84193e02e67164b9b4402e47ef9cffa5b09": "/static/icons/weth.svg",
+    "0xddd076e315e288a7146e8c8612a57baae4df21c7": "/static/icons/usdt.svg"
+  },
+  "80001": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/matic.svg",
+    "0xbbb02de94e1dd1e99b43458027f31abb2d75659b": "/static/icons/wbtc.svg",
+    "0xeee0c84193e02e67164b9b4402e47ef9cffa5b09": "/static/icons/weth.svg",
+    "0xddd076e315e288a7146e8c8612a57baae4df21c7": "/static/icons/usdt.svg"
+  },
+  "421613": {
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee": "/static/icons/eth.svg",
+    "0xbbb02de94e1dd1e99b43458027f31abb2d75659b": "/static/icons/wbtc.svg",
+    "0xeee0c84193e02e67164b9b4402e47ef9cffa5b09": "/static/icons/weth.svg",
+    "0xddd076e315e288a7146e8c8612a57baae4df21c7": "/static/icons/usdt.svg"
+  }
+}
